14e923e63d50d25f9007bac1452c7d27c59fa537
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-03-13 07:39:38 -0500

    update

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 // alert("hi");
 // serlectors
-const images = document.querySelectorAll("img");
 
-images.addEventListener("mouseover", function () {
-  images.style.width = "200px";
-  images.style.height = "200px";
-});
+const images = document.querySelectorAll("img"); //
+const links = document.querySelectorAll("a");
+
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener("mouseover", function () {
+    images[i].style.width = "200px";
+    images[i].style.height = "200px";
+  });
+  setTimeout(function () {
+    images[i].style.width = "150px";
+    images[i].style.height = "150px";
+  }, 3000);
+}
+
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", function () {
+    images[i].style.width = "150px";
+    images[i].style.height = "150px";
+  });
+}
+
+setTimeout(function () {
+  images[i].style.width = "150px";
+  images[i].style.height = "150px";
+}, 3000);
